753334f118380903bf859f9e0da02391ffd86b7c
NjuptMei <824931597@qq.com>
2023-05-08 10:56:29 +0800

    Initial commit

---
+++ src/main/java/com/mc/rediss/RedisStarterApplication.java
@@ -0,0 +1,13 @@
+package com.mc.rediss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mc/rediss/RedisStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.rediss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
